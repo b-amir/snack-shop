c23664585b3829e42d44e48e37bed6e68f9956e0
theamirm <theamirm@gmail.com>
2025-04-26 15:48:09 +0330

    feat: validate locale and show 404 for unsupported locales

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,14 @@
 import { ProductList } from "@/components/products/ProductList";
 import styles from "./page.module.css";
+import { notFound } from "next/navigation";
 
 export default async function Home(props: { params: { locale: string } }) {
   const params = await props.params;
   const { locale } = params;
+  const supportedLocales = ["en", "fa"];
+  if (!supportedLocales.includes(locale)) {
+    notFound();
+  }
   return (
     <div className={styles.container}>
       <ProductList locale={locale} />
