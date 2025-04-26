918af98d4acc3d8923dff7e18abd7c5caf91d024
theamirm <theamirm@gmail.com>
2025-04-26 03:50:23 +0330

    feat: update layout and page components with new structure

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,4 @@
-import { ProductList } from "../components/products/ProductList";
+import { ProductList } from "@/components/products/ProductList";
 import styles from "./page.module.css";
 
 export default function Home() {
