ef07c96245e152be9976282d23ef3ea5a3e30012
theamirm <theamirm@gmail.com>
2025-04-26 04:47:22 +0330

    refactor: migrate LocaleNumber and convertToFarsiDigits to TypeScript

--- src/components/LocaleNumber.js
+++
@@ -1,8 +0,0 @@
-import { toFarsiDigits } from "../utils/convertToFarsiDigits";
-import { useLocale } from "next-intl";
-
-export default function LocaleNumber({ children }) {
-  const locale = useLocale?.() || "en";
-  const str = String(children);
-  return locale === "fa" ? toFarsiDigits(str) : str;
-}
--- src/utils/convertToFarsiDigits.js
+++
@@ -1,5 +0,0 @@
-export function toFarsiDigits(strOrNumber) {
-  const str = String(strOrNumber);
-  const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
-  return str.replace(/\d/g, (d) => farsiDigits[d]);
-}
