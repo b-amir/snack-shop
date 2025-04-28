0c361acd501bcb0d17776fd22a0a891c36c41d79
theamirm <theamirm@gmail.com>
2025-04-28 23:45:01 +0330

    feat: update Footer with caching, revalidation button, and styles

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,10 +1,17 @@
 import { LanguageSwitcher } from "./LanguageSwitcher";
 import styles from "./styles.module.css";
+import { RevalidationButton } from "../CacheRevalidationControl";
+import { getDirection } from "@/utils/direction";
 
 export function Footer({ locale }: { locale: string }) {
+  const dir = getDirection(locale);
+  const contentStyle =
+    dir === "rtl" ? styles.footerContentRtl : styles.footerContentLtr;
+
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footerContent}>
+    <footer className={styles.footer} dir={dir}>
+      <div className={contentStyle}>
+        <RevalidationButton />
         <LanguageSwitcher locale={locale} />
       </div>
     </footer>
